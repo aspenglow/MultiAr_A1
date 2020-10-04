62c721d87f7d596edb779d8289b3512ae9587ea3
aspenglow <71600960+aspenglow@users.noreply.github.com>
2020-10-04 11:40:58 -0700

    Add files via upload

---
+++ integral.c
@@ -0,0 +1,63 @@
+/*
+============================================================================
+Filename    : integral.c
+Author      : Your names goes here
+SCIPER		: Your SCIPER numbers
+============================================================================
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "utility.h"
+#include "function.c"
+
+double integrate (int num_threads, int samples, int a, int b, double (*f)(double));
+
+int main (int argc, const char *argv[]) {
+
+    int num_threads, num_samples, a, b;
+    double integral;
+
+    if (argc != 5) {
+        num_threads = 1;
+        num_samples = 5000000;
+        a = 5;
+        b = 9;
+        // printf("Invalid input! Usage: ./integral <num_threads> <num_samples> <a> <b>\n");
+		// return 1;
+	} else {
+        num_threads = atoi(argv[1]);
+        num_samples = atoi(argv[2]);
+        a = atoi(argv[3]);
+        b = atoi(argv[4]);
+	}
+
+    set_clock();
+
+    /* You can use your self-defined funtions by replacing identity_f. */
+    integral = integrate (num_threads, num_samples, a, b, identity_f);
+
+    printf("- Using %d threads: integral on [%d,%d] = %.15g computed in %.4gs.\n", num_threads, a, b, integral, elapsed_time());
+
+    return 0;
+}
+
+
+double integrate (int num_threads, int samples, int a, int b, double (*f)(double)) {
+    double integral;
+    /* Your code goes here */
+    //double pixel;
+    int i;
+    double domain = (double)(b-a);
+    rand_gen gen = init_rand();
+    double sum = 0;
+
+#pragma omp parallel for num_threads(num_threads) reduction(+:sum) shared(samples) private(i)
+    for (i = 0; i < samples; i++) {
+        double x = next_rand(gen);
+        x = (double)a+ domain * x;
+        sum += f(x);
+    }
+    integral = sum / (double)samples * domain;
+    return integral;
+}
---
+++ pi.c
@@ -0,0 +1,63 @@
+/*
+============================================================================
+Filename    : pi.c
+Author      : Your names goes here
+SCIPER		: Your SCIPER numbers
+============================================================================
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "utility.h"
+
+double calculate_pi (int num_threads, int samples);
+
+int main (int argc, const char *argv[]) {
+
+    int num_threads, num_samples;
+    double pi;
+
+    if (argc != 3) {
+        num_threads = 4;
+        num_samples = 500000;
+		//printf("Invalid input! Usage: ./pi <num_threads> <num_samples> \n");
+		//return 1;
+	} else {
+        num_threads = atoi(argv[1]);
+        num_samples = atoi(argv[2]);
+	}
+
+    set_clock();
+    pi = calculate_pi (num_threads, num_samples);
+
+    printf("- Using %d threads: pi = %.15g computed in %.4gs.\n", num_threads, pi, elapsed_time());
+
+    return 0;
+}
+
+
+double calculate_pi (int num_threads, int samples) {
+    double pi;
+    /* Your code goes here */
+
+    int i;
+    int tid;
+    //double x, y;
+
+    int count = 0, total = 0;
+    rand_gen gen = init_rand();
+    omp_set_num_threads(num_threads);
+
+#pragma omp parallel for num_threads(num_threads) \
+    reduction(+:count) shared(samples) private(i)
+    for (i = 0; i < samples; i++) {
+        double x = next_rand(gen);
+        double y = next_rand(gen);
+        if (x * x + y * y <= 1) {
+            count++;
+        }
+    }
+    free_rand(gen);
+    pi = (double)count / samples * 4;
+    return pi;
+}
